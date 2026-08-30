9b0beb77ce1f930320e7909617eebbb37aa067b5
bimannandib <bimannandib@gmail.com>
2026-08-30 16:09:11 +0530

    added default time

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -10,7 +10,7 @@ export const taskSchema = new Schema({
   isCompleted: {type: Boolean, default: false},
   completedAt: {type: Date, default: null},
   date: {type: Date, default: Date.now},
-  time: {type: String}
+  time: {type: String, default: "00:00"}
 })
 
 export const Tasks = mongoose.model('Tasks', taskSchema)
